0c54a772eee96c2f12fe2e5a0ce56f002b373b10
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-02-14 01:56:57 +0800

    更改文章封面显示;优化js，使用es6语法

--- source/js/common.js
+++ source/js/common.js
@@ -1,6 +1,6 @@
 window.$claudia = {
     throttle: function (func, time) {
-        var wait = false
+        let wait = false
         return function () {
             if (wait) return
             wait = true
@@ -11,11 +11,11 @@ window.$claudia = {
             }, time || 100)
         }
     },
-    fadeInImage: function(imgs, imageLoadedCallback) {
-        var images = imgs || document.querySelectorAll('.js-img-fadeIn')
+    fadeInImage: function (imgs, imageLoadedCallback) {
+        const images = imgs || document.querySelectorAll('.js-img-fadeIn')
 
         function loaded(event) {
-            var image = event.currentTarget
+            const image = event.currentTarget
             image.classList.add('show')
             if (image.parentElement && image.parentElement.classList.contains('skeleton')) {
                 image.parentElement.classList.remove('skeleton')
@@ -31,21 +31,38 @@ window.$claudia = {
             img.addEventListener('load', loaded)
         })
     },
-    blurBackdropImg: function(image) {
+    blurBackdropImg: function (image) {
         if (!image.dataset.backdrop) return
 
-        var parent = image.parentElement //TODO: Not finish yes, must be a pure function
-        var parentWidth = Math.round(parent.getBoundingClientRect().width)
-        var childImgWidth = Math.round(image.getBoundingClientRect().width)
+        const parent = image.parentElement //TODO: Not finish yes, must be a pure function
+        const parentWidth = Math.round(parent.getBoundingClientRect().width)
+        const childImgWidth = Math.round(image.getBoundingClientRect().width)
+        const vw20 = screen.availWidth * 0.2
+        const isCovered = (parentWidth - childImgWidth) < vw20
+        const blurImg = parent.previousElementSibling //TODO: Not finish yes, must be a pure function
 
-        var isCovered = parentWidth === childImgWidth
-        var blurImg = parent.previousElementSibling //TODO: Not finish yes, must be a pure function
-
-        isCovered ? blurImg.classList.add('is-hidden') : blurImg.classList.remove('is-hidden')
+        isCovered ? (blurImg.classList.add('is-hidden'), $claudia.adjust(parent, image, true)) : (blurImg.classList.remove('is-hidden'), $claudia.adjust(parent, image, false))
+    },
+    /**
+     * 
+     * @param {HTMLElement} element 
+     * @param {HTMLImageElement} img
+     * @param {boolean} adjustOn
+     */
+    adjust: (parent, img, adjustOn) => {
+        if (adjustOn) {
+            img.classList.remove('show')
+            parent.style.backgroundImage = `url(${img.src})`
+            parent.classList.add('img-cover-centre')
+        } else {
+            img.classList.add('show')
+            parent.style.backgroundImage = ''
+            parent.classList.remove('img-cover-centre')
+        }
     },
     getSystemTheme(callback) {
-        var media = window.matchMedia('(prefers-color-scheme: dark)')
-        media.addEventListener('change', function (e){
+        const media = window.matchMedia('(prefers-color-scheme: dark)')
+        media.addEventListener('change', function (e) {
             callback && callback(e.matches ? "dark" : "light")
         })
 
